c156dee937371a8bda8c891d65c5d8f47675648b
liuliu <1091267866@qq.con>
2019-12-02 16:49:33 +0800

    美化后的版本

--- new/app/src/main/java/com/example/myapplication/homework.java
+++ new/app/src/main/java/com/example/myapplication/homework.java
@@ -7,9 +7,31 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.view.MenuItem;
 import android.widget.TextView;
+import android.support.v4.view.ViewPager;
+import java.util.ArrayList;
+
 
 public class homework extends AppCompatActivity {
     private TextView mTextMessage;
+
+    //粘贴的代码
+    //定义一个访问图片的数组
+
+    int [] images = new int []{
+            R.drawable.p6,
+            R.drawable.p1,
+            R.drawable.p2,
+            R.drawable.p3,
+            R.drawable.p4,
+            R.drawable.p5,
+            R.drawable.p6,
+            R.drawable.p1,
+    };
+    private ViewPager vp;
+    private ArrayList<String> urls = new ArrayList<>();
+    private LoopVPAdapter loopVPAdapter;
+
+//粘贴的代码
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -39,9 +61,25 @@ public class homework extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homework);
+
+        //粘贴的代码
+        urls.add("http://seopic.699pic.com/photo/00005/5186.jpg_wh1200.jpg");
+        urls.add("http://seopic.699pic.com/photo/50010/0719.jpg_wh1200.jpg");
+        urls.add("http://seopic.699pic.com/photo/50009/9449.jpg_wh1200.jpg");
+        urls.add("http://seopic.699pic.com/photo/50002/5923.jpg_wh1200.jpg");
+        urls.add("http://seopic.699pic.com/photo/50001/9330.jpg_wh1200.jpg");
+        urls.add("http://seopic.699pic.com/photo/50009/9191.jpg_wh1200.jpg");
+        vp = (ViewPager) findViewById(R.id.vp);
+        loopVPAdapter = new ImgAdapter(this,urls,vp);
+
+        //粘贴的代码
         BottomNavigationView navView = findViewById(R.id.nav_view);
         mTextMessage = findViewById(R.id.message);
         navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
+    //粘贴的代码
 
+
+
+    //粘贴的代码
 }
